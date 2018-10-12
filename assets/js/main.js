8bc05fec1a457e2996e07ca59dc9523f991c8f81
Zac McClung <zac.mcclung370@gmail.com>
2018-10-11 21:16:23 -0400

    Saving changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -223,7 +223,7 @@ function audioPlayer() {
   let currentSong = 0;
 
   $('#audioPlayer')[0].src = $('#playlist li a')[0];
-  // $('#audioPlayer')[0].play();
+  $('#audioPlayer')[0].play();
   $('#playlist li a').click(function(e) {
     e.preventDefault();
     $('#audioPlayer')[0].src = this;
